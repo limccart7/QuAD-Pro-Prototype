efbbd427c618a183757adadc3f0fafa8293d2df0
limccart7 <145800942+limccart7@users.noreply.github.com>
2024-09-10 15:21:28 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+# install script for Project-OWL quad pro
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install unzip
+sudo apt-get install libhiredis-dev
+
+sudo apt-get update
+sudo apt-get install redis-server
+sudo apt-get install -y cmake 
+
+wget http://abyz.me.uk/lg/lg.zip
+unzip lg.zip
+cd lg
+make
+sudo make install
+cd ..
+rm -rf lg
+
+
